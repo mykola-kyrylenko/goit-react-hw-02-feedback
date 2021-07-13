6c0590d074035d7cb550cb53d0526becee1fc1e2
Mykola Kyrylenko <bulletbest@gmail.com>
2021-07-13 23:38:10 +0300

    some fix

--- src/components/FeedBack.js
+++ src/components/FeedBack.js
@@ -10,8 +10,8 @@ class FeedBack extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
-    percentage: 0,
+    // total: 0,
+    // percentage: 0,
   };
 
   countPercentage = () => {
